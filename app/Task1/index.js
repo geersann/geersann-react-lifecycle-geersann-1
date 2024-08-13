9656c4718a00856a37adda0b3e91860a89bec6ef
geersann <148795588+geersann@users.noreply.github.com>
2024-08-13 01:33:54 +0000

    add fetch in index.js

--- app/Task1/index.js
+++ app/Task1/index.js
@@ -5,8 +5,51 @@ export default class Task1 extends Component {
         super(props);
 
         this.state = {
-            list: null
+            list: null,
+            loading: true,
+            error: null
         };
     }
 
+    componentDidMount() {
+        fetch('http://localhost:3000/list')
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('Network not responding');
+                }
+                return response.json();
+            })
+            .then(list => this.setState({ list, loading: false }))
+            .catch(error => this.setState({ error, loading: false }));
+    }
+
+    render() {
+        const { list, loading, error } = this.state;
+
+        if (loading) {
+            return (
+                <div className='block2' 
+                     style={{ borderStyle: 'solid', borderWidth: '1px'}}>
+                    Loading...
+                </div>
+            );
+        }
+
+        if (error) {
+            return <div className='block2'>Error: {error.message}</div>;
+        }
+
+        return (
+            <div className='block1'>
+                <h1>Show List</h1>
+                <ul>
+                    {list.map((item, index) => (
+                        <li key={index}>
+                            {item.name} - {item.note}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
